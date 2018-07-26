85c863be170da3d170c656e4ee316bb2378b7eb1
Kotaro Sugawara <kotarella1110@gmail.com>
2018-07-26 13:36:31 +0900

    Fixed Atom TodoText component styled-components でスタイリング

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   processors: ['stylelint-processor-styled-components'],
   extends: ['stylelint-config-airbnb', 'stylelint-config-styled-components'],
+  rules: {
+    'number-leading-zero': 'always',
+  },
 };
--- src/components/atoms/TodoText/index.js
+++ src/components/atoms/TodoText/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const TodoText = () => <input type="text" />;
+const InputText = styled.input.attrs({
+  type: 'text',
+})`
+  padding: 16px 16px 16px 60px;
+  border: 0;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+`;
+
+const TodoText = () => <InputText />;
 
 export default TodoText;
