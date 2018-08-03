507fad9276f111ef4e9230ce1dba94041f9bbe37
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-03 09:23:00 +0900

    Fixed .storybook/config.js styled-components の injectGlobal を追加

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,36 @@
 import { setStubbingMode } from 'react-stubber';
+import { injectGlobal } from 'styled-components';
 import { configure, setAddon } from '@storybook/react';
 import '@storybook/addon-console';
 import { setOptions } from '@storybook/addon-options/preview';
 import JSXAddon from 'storybook-addon-jsx';
 
 setStubbingMode(true);
+
+injectGlobal([
+  `
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    line-height: 1.4em;
+    background: #f5f5f5;
+    color: #4d4d4d;
+    min-width: 230px;
+    max-width: 550px;
+    margin: 0 auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-weight: 300;
+  }
+  :focus {
+    outline: 0;
+  }
+`,
+]);
 
 setOptions({
   hierarchySeparator: /\//,
