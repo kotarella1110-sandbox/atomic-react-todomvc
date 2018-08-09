a1ffec6fd17cf26d88e15625fcf753e55a9136bb
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-09 13:26:48 +0900

    Added Organism TodoList component

---
+++ src/components/organisms/TodoList/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TodoList } from 'components';
+
+const setup = () => {
+  const props = {};
+
+  const wrapper = shallow(<TodoList {...props} />);
+
+  return {
+    props,
+    wrapper,
+  };
+};
+
+describe('TodoList', () => {
+  it('コンポーネントがレンダリングされていること', () => {
+    const { wrapper } = setup();
+    expect(wrapper.type()).toBe('ul');
+  });
+});
