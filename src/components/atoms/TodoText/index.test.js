c776b45a08d7394d57a5145e89b009c5c9bf53f5
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-09 16:13:47 +0900

    Fixed test about Atom TodoText component wrap() から setup() に修正

--- src/components/atoms/TodoText/index.test.js
+++ src/components/atoms/TodoText/index.test.js
@@ -1,36 +1,56 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TodoText from '.';
+import { TodoText } from 'components';
+
+const setup = propOverrides => {
+  const props = Object.assign(
+    {
+      handleEnter: jest.fn(),
+      text: 'foo',
+      placeholder: 'foo',
+      editing: false,
+      newTodo: false,
+    },
+    propOverrides
+  );
+
+  const wrapper = shallow(<TodoText {...props} />);
+
+  return {
+    props,
+    wrapper,
+  };
+};
 
 describe('TodoText', () => {
-  const wrap = (props = {}) => shallow(<TodoText {...props} />);
-
   it('コンポーネントがレンダリングされていること', () => {
-    const wrapper = wrap();
+    const { wrapper } = setup();
     expect(wrapper.exists()).toBe(true);
   });
 
   it('InputText コンポーネントがレンダリングされていること', () => {
-    const wrapper = wrap();
+    const { wrapper } = setup();
     expect(wrapper.find('InputText').exists()).toBe(true);
   });
 
   it('Props で渡したテキストが State に保持されていること', () => {
-    const wrapper = wrap({ text: 'Test!' });
-    expect(wrapper.state('text')).toBe('Test!');
+    const { wrapper } = setup();
+    expect(wrapper.state('text')).toBe('foo');
   });
 
   it('入力したテキストが State に保持されていること', () => {
-    const wrapper = wrap({ text: 'Test!' });
-    wrapper.simulate('change', { target: { value: 'Test! Input!' } });
-    expect(wrapper.state('text')).toBe('Test! Input!');
+    const { wrapper } = setup();
+    wrapper.simulate('change', { target: { value: 'bar' } });
+    expect(wrapper.state('text')).toBe('bar');
   });
 
   it('Enter を押すと入力されたテキストを引数として Props で渡されたコールバックを実行すること', () => {
-    const handleEnter = jest.fn();
-    const wrapper = wrap({ text: 'Test!', handleEnter });
+    const {
+      wrapper,
+      props: { handleEnter },
+    } = setup();
     wrapper.find('InputText').simulate('keyDown', { keyCode: 13 });
     expect(handleEnter).toHaveBeenCalledTimes(1);
-    expect(handleEnter).toHaveBeenCalledWith('Test!');
+    expect(handleEnter).toHaveBeenCalledWith('foo');
   });
 });
