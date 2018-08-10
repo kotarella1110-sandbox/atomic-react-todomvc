fe007d59e91dc3c5b857016b8778428bf6d25454
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-10 10:56:02 +0900

    Fixed style about Atom TodoText component

--- src/components/atoms/TodoText/index.js
+++ src/components/atoms/TodoText/index.js
@@ -1,15 +1,35 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
 const InputText = styled.input.attrs({
   type: 'text',
 })`
-  padding: 16px 16px 16px 60px;
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
   border: 0;
-  background: rgba(0, 0, 0, 0.003);
-  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  color: inherit;
+  padding: 6px;
+  border: 1px solid #999;
+  box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  ${({ editing }) =>
+    !editing &&
+    css`
+      padding: 16px 16px 16px 60px;
+      border: 0;
+      background: rgba(0, 0, 0, 0.003);
+      box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+    `};
 `;
 
 class TodoText extends React.Component {
