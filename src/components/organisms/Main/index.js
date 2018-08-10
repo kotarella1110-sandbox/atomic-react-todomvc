ad90a744571321ccb7f36a50b5741ca25305e9d3
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-10 18:11:06 +0900

    Fixed Organism Main component
    PropTypes 追加

--- src/components/organisms/Main/index.js
+++ src/components/organisms/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import { TodoList } from 'components';
 
 const Wrapper = styled.div`
@@ -12,5 +13,18 @@ const Main = props => (
     <TodoList {...props} />
   </Wrapper>
 );
+
+Main.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
+  completeTodo: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
+};
 
 export default Main;
