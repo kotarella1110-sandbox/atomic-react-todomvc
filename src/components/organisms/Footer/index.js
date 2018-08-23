e7d4764355c7bce6d5b5edcdf00c4d2639435331
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-23 14:58:03 +0900

    Added style for Organism Footer component

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -1,21 +1,89 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import { Button } from 'components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  color: #777;
+  padding: 10px 15px;
+  height: 20px;
+  text-align: center;
+  border-top: 1px solid #e6e6e6;
 
-const TodoCount = styled.span``;
+  @media (max-width: 430px) {
+    height: 50px;
+  }
 
-const TodoCountNumber = styled.strong``;
+  &:before {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 50px;
+    overflow: hidden;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6,
+      0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6,
+      0 17px 2px -6px rgba(0, 0, 0, 0.2);
+  }
+`;
 
-const Filters = styled.ul``;
+const TodoCount = styled.span`
+  float: left;
+  text-align: left;
+`;
 
-const FilterItem = styled.li``;
+const TodoCountNumber = styled.strong`
+  font-weight: 300;
+`;
 
-const FilterItemLink = styled.a``;
+const Filters = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  position: absolute;
+  right: 0;
+  left: 0;
 
-const ClearCompletedButton = styled(Button)``;
+  @media (max-width: 430px) {
+    bottom: 10px;
+  }
+`;
+
+const FilterItem = styled.li`
+  display: inline;
+`;
+
+const FilterItemLink = styled.a`
+  color: inherit;
+  margin: 3px;
+  padding: 3px 7px;
+  text-decoration: none;
+  border: 1px solid transparent;
+  border-radius: 3px;
+
+  &:hover {
+    border-color: rgba(175, 47, 47, 0.1);
+  }
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      border-color: rgba(175, 47, 47, 0.2);
+    `};
+`;
+
+const ClearCompletedButton = styled(Button)`
+  float: right;
+  position: relative;
+  line-height: 20px;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const FILTER_TITLES = {
   SHOW_ALL: 'All',
