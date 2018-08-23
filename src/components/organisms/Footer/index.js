ad90144454372dd162618a4e6b9b488d22dfffac
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-23 10:53:00 +0900

    Added test for Organism Footer component Filters コンポーネントがレンダリングされていること

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -7,11 +7,30 @@ const TodoCount = styled.span``;
 
 const TodoCountNumber = styled.strong``;
 
+const Filters = styled.ul``;
+
+const FilterItem = styled.li``;
+
+const FilterItemLink = styled.a``;
+
+const FILTER_TITLES = {
+  SHOW_ALL: 'All',
+  SHOW_ACTIVE: 'Active',
+  SHOW_COMPLETED: 'Completed',
+};
+
 const Footer = () => (
   <Wrapper>
     <TodoCount>
       <TodoCountNumber />
     </TodoCount>
+    <Filters>
+      {Object.keys(FILTER_TITLES).map(filter => (
+        <FilterItem key={filter}>
+          <FilterItemLink>{FILTER_TITLES[filter]}</FilterItemLink>.
+        </FilterItem>
+      ))}
+    </Filters>
   </Wrapper>
 );
 
--- src/components/organisms/Footer/index.test.js
+++ src/components/organisms/Footer/index.test.js
@@ -16,8 +16,26 @@ describe('Footer', () => {
     expect(wrapper.dive().type()).toBe('div');
 
     const TodoCount = wrapper.dive().find('TodoCount');
-    const TodoCountNumber = TodoCount.dive().find('TodoCountNumber');
     expect(TodoCount.dive().type()).toBe('span');
+
+    const TodoCountNumber = TodoCount.dive().find('TodoCountNumber');
     expect(TodoCountNumber.dive().type()).toBe('strong');
+
+    const todoFilters = ['SHOW_ALL', 'SHOW_ACTIVE', 'SHOW_COMPLETED'];
+    const filterTitles = ['All', 'Active', 'Completed'];
+    const Filters = wrapper.dive().find('Filters');
+    expect(Filters.dive().type()).toBe('ul');
+    expect(Filters.dive().children().length).toBe(3);
+    Filters.dive()
+      .children()
+      .forEach((FilterItem, index) => {
+        const FilterItemLink = FilterItem.dive().find('FilterItemLink');
+        expect(FilterItem.key()).toBe(todoFilters[index]);
+        expect(FilterItem.dive().type()).toBe('li');
+        expect(FilterItemLink.dive().type()).toBe('a');
+        expect(FilterItemLink.dive().prop('children')).toBe(
+          filterTitles[index]
+        );
+      });
   });
 });
