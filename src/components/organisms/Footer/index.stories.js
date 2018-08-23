f9e8734460b4429ea3de3583ea8d053473fbcf27
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-23 14:55:51 +0900

    Added story for Organism Footer component

--- src/components/organisms/Footer/index.stories.js
+++ src/components/organisms/Footer/index.stories.js
@@ -28,4 +28,40 @@ storiesOf('Organism|Footer', module)
         onClearCompleted={action('ClearCompleted')}
       />
     ))
+  )
+  .addWithJSX(
+    'show all and completed count 1 and active count 1',
+    withNotes(``)(() => (
+      <Footer
+        visibilityFilter="SHOW_ALL"
+        completedCount={1}
+        activeCount={1}
+        setVisibilityFilter={action('SetVisibilityFilter')}
+        onClearCompleted={action('ClearCompleted')}
+      />
+    ))
+  )
+  .addWithJSX(
+    'show active and completed count 0 and active count 2',
+    withNotes(``)(() => (
+      <Footer
+        visibilityFilter="SHOW_ACTIVE"
+        completedCount={0}
+        activeCount={2}
+        setVisibilityFilter={action('SetVisibilityFilter')}
+        onClearCompleted={action('ClearCompleted')}
+      />
+    ))
+  )
+  .addWithJSX(
+    'show completed and completed count 2 and active count 0',
+    withNotes(``)(() => (
+      <Footer
+        visibilityFilter="SHOW_COMPLETED"
+        completedCount={2}
+        activeCount={0}
+        setVisibilityFilter={action('SetVisibilityFilter')}
+        onClearCompleted={action('ClearCompleted')}
+      />
+    ))
   );
