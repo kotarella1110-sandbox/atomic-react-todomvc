db7de9337e8c39f55bc5136322e5b71a681e4882
Kotaro Sugawara <kotarella1110@gmail.com>
2018-08-23 15:35:24 +0900

    Added styling for Tempalte TodoTemplate component

--- src/components/templates/TodoTemplate/index.js
+++ src/components/templates/TodoTemplate/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Title = styled.h1`
+  font-size: 100px;
+  font-weight: 100;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+`;
 
-const Title = styled.h1``;
+const Wrapper = styled.div`
+  background: #fff;
+  margin: 130px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+
+  ${Title} {
+    position: absolute;
+    top: -155px;
+    width: 100%;
+    text-align: center;
+  }
+`;
 
 const Header = styled.header``;
 
